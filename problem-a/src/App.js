ad193f8a49065642ffe50d80278f615bddb8e372
Rishi <rishi.kavikondala@gmail.com>
2020-11-24 16:36:22 -0800

    Finished problem set 8

--- problem-a/src/App.js
+++ problem-a/src/App.js
@@ -6,3 +6,65 @@ const EXAMPLE_SENATORS = [
 ];
 
 /* Your code goes here */
+
+export function SenatorRow(props) {
+  let senator = props.senator;
+  let senatorName = senator.name;
+  let senatorStateParty = props.senator.party.slice(0, 1) + " - " + senator.state;
+  let senatorPhoneNum = senator.phone;
+  let senatorPhoneNumForHref = "tel:" + senator.phone;
+  let senatorTwitter = "https://twitter.com/" + senator.twitter;
+  let senatorTwitterHandle = "@" + senator.twitter;
+  return (
+    <tr>
+      <td>{senatorName}</td>
+      <td>{senatorStateParty}</td>
+      <td><a href={senatorPhoneNumForHref}>{senatorPhoneNum}</a></td>
+      <td><a href={senatorTwitter}>{senatorTwitterHandle}</a></td>
+    </tr>
+  )
+}
+
+export function TableHeader(props) {
+  let columnNames = props.columnNames;
+  let tableElements = columnNames.map((name) => {
+    let thisTH = <th key={name}>{name}</th> 
+    return thisTH;
+  })
+  return (
+    <thead>
+      <tr>
+        {tableElements}
+      </tr>
+    </thead>
+  )
+}
+
+export function SenatorTable(props) {
+  let columnNames = ['Name', 'State', 'Phone', 'Twitter'];
+  let senatorRows = props.senators.map((senator) => {
+    let thisSenator = <SenatorRow senator={senator} key={senator.id}/>
+    return (
+      <tbody>
+        {thisSenator}
+      </tbody>
+    )
+  })
+  return (
+    <table className="table table-bordered">
+      <TableHeader columnNames={columnNames} />
+        {senatorRows}
+    </table>
+  )
+}
+
+export function App(props) {
+  return (
+    <div className="container">
+      <h1>US Senators (Oct 2020)</h1>
+      <SenatorTable senators={props.senators}/>
+    </div>
+  )
+}
+
+export default App;
--- problem-b/src/App.js
+++ problem-b/src/App.js
@@ -1,9 +1,126 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function App() {
+import './style.css';
+
+export function App(props) {
+  const[pets, setPets] = useState(props.pets);
+  const handleAdopt = function(name) {
+    let updatedPets = pets.map((pet) => {
+      if(pet.name === name) {
+        pet.adopted = true;
+      }
+      return pet;
+    })
+    setPets(updatedPets);
+  }
   return (
-    <div>Replace this with your content!</div>
+    <div>
+      <HeaderSection/>,
+      <MainSection pets={props.pets} adoptCallback={handleAdopt}/>,
+      <FooterSection/>
+    </div>
   );
+}
+
+export function HeaderSection() {
+  return (
+    <header className="jumbotron jumbotron-fluid py-4">
+      <div className="container">
+        <h1>Adopt a Pet</h1>
+      </div>
+    </header>
+  )
+}
+
+export function MainSection(props) {
+  let breeds = ["Breed A", "Breed B"];
+  return (
+    <main className="container">
+      <div className="row">
+        <div id="navs" className="col-3">
+          <BreedNav breeds={breeds}/>
+
+          <AboutNav/>
+        </div>
+
+        <PetList pets={props.pets} adoptCallback={props.adoptCallback}/>
+
+      </div>
+    </main>
+  )
+}
+
+export function FooterSection() {
+  return (
+    <footer className="container">
+      <small>Images from <a href="http://www.seattlehumane.org/adoption/dogs">Seattle Humane Society</a></small>
+    </footer>
+  )
+}
+
+export function AboutNav() {
+  return (
+    <nav id="aboutLinks">
+      <h2>About</h2>
+      <ul className="list-unstyled">
+        <li><a href="#/">How to Adopt</a></li>
+        <li><a href="#/">Volunteering</a></li>
+        <li><a href="#/">Events</a></li>
+        <li><a href="#/">Donate</a></li>
+        <li><a href="#/">About Us</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export function BreedNav(props) {
+  let breeds = props.breeds;
+  let breedList = breeds.map((breed) => {
+    let thisBreed = <li><a key={breed} href={breed}>{breed}</a></li>
+    return thisBreed;
+  })
+  return (
+    <nav id="breedLinks">
+      <h2>Pick a Breed</h2>
+      <ul className="list-unstyled">
+        {breedList}
+      </ul>
+    </nav>
+  )
+}
+
+export function PetCard(props) {
+  let petCard = props.petCard;
+  let petName = petCard.name;
+  let petImage = petCard.img;
+  let petGenderBreed = petCard.sex + " " + petCard.breed;
+  const handleClick = () => {props.adoptCallback(petName)};
+  if(petCard.adopted) {
+    petName = petName + " (Adopted)";
+  }
+  return (
+    <div className="card" onClick={handleClick}>
+      <img className="card-img-top" src={petImage} alt={petName} />
+      <div className="card-body">
+        <h3 className="card-title">{petName}</h3>
+        <p className="card-text">{petGenderBreed}</p>
+      </div>
+    </div>
+  )
+}
+
+export function PetList(props) {
+  let petsArray = props.pets;
+  let petsList = petsArray.map((pet) => {
+    let thisPet = <PetCard petCard={pet} adoptCallback={props.adoptCallback}/>
+    return thisPet;
+  })
+  return (
+    <div id="petList" className="col-9">
+      <h2>Dogs for Adoption</h2>
+      {petsList}
+    </div>
+  )
 }
 
 export default App;
